3ba1b16c59b25b9a6abe6f4a1dbc4091dac0cdbd
s344951241 <344951241@qq.com>
2018-06-21 11:31:54 +0800

    11

---
+++ Assets/Scripts/RotateSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class RotateSystem : ComponentSystem {
+	struct Components
+	{
+		public Rotator rotator;
+		public Transform transform;
+	}
+	
+	protected override void OnUpdate()
+	{
+        foreach (var item in GetEntities<Components>())
+        {
+            item.transform.Rotate(0f, item.rotator.speed * Time.deltaTime, 0f);
+        }
+
+    }
+
+}
+
+
+
